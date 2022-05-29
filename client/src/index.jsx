947ac6979b08e1b030252e7b9d74b606b96947ff
Frank Mathy <frankmathy@gmail.com>
2022-05-29 20:40:59 +0200

    Fixed bugs

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles.styles';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <>
     <GlobalStyles />
     <App />
-  </>,
-  document.getElementById('root')
+  </>
 );
